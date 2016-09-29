fb7a615ebec0884bc9072a0c1c1d98f1cb52fab0
Patrick Wolfram <pwolfram@geontech.com>
2016-09-29 19:37:26 -0400

    Modified component main to allow for launching as shared library.

--- cpp_armv7l/main.cpp
+++ cpp_armv7l/main.cpp
@@ -1,11 +1,48 @@
 #include <iostream>
 #include "ossie/ossieSupport.h"
+#include <ossie/Device_impl.h>
 
 #include "RFNoC_TestComponent.h"
+
+RFNoC_TestComponent_i *resourcePtr;
+
+void signal_catcher(int sig)
+{
+    // IMPORTANT Don't call exit(...) in this function
+    // issue all CORBA calls that you need for cleanup here before calling ORB shutdown
+    if (resourcePtr) {
+        resourcePtr->halt();
+    }
+}
 int main(int argc, char* argv[])
 {
-    RFNoC_TestComponent_i* RFNoC_TestComponent_servant;
-    Component::start_component(RFNoC_TestComponent_servant, argc, argv);
+    struct sigaction sa;
+    sa.sa_handler = signal_catcher;
+    sa.sa_flags = 0;
+    resourcePtr = 0;
+
+    Component::start_component(resourcePtr, argc, argv);
     return 0;
+}
+
+extern "C" {
+    Resource_impl* construct(int argc, char* argv[], Device_impl* parentDevice) {
+
+        struct sigaction sa;
+        sa.sa_handler = signal_catcher;
+        sa.sa_flags = 0;
+        resourcePtr = 0;
+
+        Component::start_component(resourcePtr, argc, argv);
+
+        // Any addition parameters passed into construct can now be
+        // set directly onto resourcePtr since it is the instantiated
+        // Redhawk device
+        //      Example:
+        //         resourcePtr->setSharedAPI(sharedAPI);
+        //resourcePtr->setParentDevice(parentDevice);
+
+        return resourcePtr;
+    }
 }
 
